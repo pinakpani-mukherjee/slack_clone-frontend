ea7cf8ae73070f07062e3019014a61b5f24cda65
pinakpani-mukherjee <mukherjee.pinakpani14@gmail.com>
2022-02-26 10:48:18 +0900

    updated index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+
+const Root = () => (
+<Router>
+  <Routes>
+    <Route path = "/" element = {<App/>}/>
+  </Routes>
+</Router>);
+  
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Root />,
   document.getElementById('root')
 );
 
